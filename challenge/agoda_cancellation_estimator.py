a88b8d76ad934c5490de792110ce41e9ba0d5290
meship <101132768+meship@users.noreply.github.com>
2022-04-01 18:10:25 +0300

    new challenge files

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 from typing import NoReturn
 from IMLearn.base import BaseEstimator
 import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import svm
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -12,63 +15,58 @@ class AgodaCancellationEstimator(BaseEstimator):
     def __init__(self) -> AgodaCancellationEstimator:
         """
         Instantiate an estimator for solving the Agoda Cancellation challenge
-
         Parameters
         ----------
-
-
         Attributes
         ----------
-
         """
         super().__init__()
+        self.estimator = SGDClassifier()
+        # self.estimator = svm.SVC()
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
         Fit an estimator for given samples
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to fit an estimator for
-
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
-
         Notes
         -----
-
         """
-        pass
+        # scaler = MinMaxScaler()
+        # scaler.fit(X)
+        # X = scaler.transform(X)
+        self.estimator = self.estimator.fit(X, y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict responses for given samples using fitted estimator
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to predict responses for
-
         Returns
         -------
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        # scaler = MinMaxScaler()
+        # scaler.fit(X)
+        # X = scaler.transform(X)
+        return self.estimator.predict(X)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
         Evaluate performance under loss function
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Test samples
-
         y : ndarray of shape (n_samples, )
             True labels of test samples
-
         Returns
         -------
         loss : float
--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 
 
+
+
 def load_data(filename: str):
     """
     Load Agoda booking cancellation dataset
@@ -12,7 +14,6 @@ def load_data(filename: str):
     ----------
     filename: str
         Path to house prices dataset
-
     Returns
     -------
     Design matrix and response vector in either of the following formats:
@@ -20,38 +21,61 @@ def load_data(filename: str):
     2) Tuple of pandas.DataFrame and Series
     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
     """
-    # TODO - replace below code with any desired preprocessing
-    full_data = pd.read_csv(filename).dropna().drop_duplicates()
-    features = full_data[["h_booking_id",
-                          "hotel_id",
-                          "accommadation_type_name",
+    full_data = pd.read_csv(filename).drop_duplicates()
+    features = full_data[[
                           "hotel_star_rating",
-                          "customer_nationality"]]
-    labels = full_data["cancellation_datetime"]
+                          "charge_option",
+                          "no_of_room",
+                            "no_of_extra_bed",
+                          "original_selling_amount",
+                          "no_of_children",
+                            "request_nonesmoke",
+                            "request_latecheckin",
+                            "request_highfloor",
+    "request_largebed","request_twinbeds", "request_airport", "request_earlycheckin"]]
+
+    # features["abroad"] = np.where(full_data["hotel_country_code"] == full_data["origin_country_code"], 0, 1)
+    features["request_nonesmoke"] = features["request_nonesmoke"].fillna(0)
+    features["request_latecheckin"] = features["request_latecheckin"].fillna(0)
+    features["request_highfloor"] = features["request_highfloor"].fillna(0)
+    features["request_largebed"] = features["request_largebed"].fillna(0)
+    features["request_twinbeds"] = features["request_twinbeds"].fillna(0)
+    features["request_airport"] = features["request_airport"].fillna(0)
+    features["request_earlycheckin"] = features["request_earlycheckin"].fillna(0)
+
+    features = features.dropna()
+
+    # grades_for_accom = {'Guest House / Bed & Breakfast': 5, 'Holiday Park / Caravan Park': 2, 'Apartment': 3,
+    #                     'Bungalow': 1, 'Love Hotel': 4,'Boat / Cruise': 9, 'Inn': 5, 'Lodge': 3,'Homestay':0,
+    #                     'Ryokan': 7, 'Resort': 9, 'Private Villa': 5, 'Tent': 1, 'Serviced Apartment': 4,
+    #                     'Capsule Hotel': 3, 'Hostel': 5, 'Hotel': 8, 'Resort Villa': 10, 'Motel': 6, 'UNKNOWN': 5,
+    #                     'Home': 0}
+    # features['accommadation_type_name'].replace(grades_for_accom, inplace=True)
+
+    grades_for_charge = {'Pay Now': 3, 'Pay Later': 2, 'Pay at Check-in': 1}
+    features['charge_option'] = features['charge_option'].replace(grades_for_charge)
+    labels = full_data['cancellation_datetime'].between("2018-07-12", "2018-13-12").astype(int)
+
 
     return features, labels
 
 
-def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
+def evaluate_and_export(estimator, X: np.ndarray, filename: str):
     """
     Export to specified file the prediction results of given estimator on given testset.
-
     File saved is in csv format with a single column named 'predicted_values' and n_samples rows containing
     predicted values.
-
     Parameters
     ----------
     estimator: BaseEstimator or any object implementing predict() method as in BaseEstimator (for example sklearn)
         Fitted estimator to use for prediction
-
     X: ndarray of shape (n_samples, n_features)
         Test design matrix to predict its responses
-
     filename:
         path to store file at
-
     """
-    pd.DataFrame(estimator.predict(X), columns=["predicted_values"]).to_csv(filename, index=False)
+    y = estimator.predict(X)
+    pd.DataFrame(y, columns=["predicted_values"]).to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
@@ -59,10 +83,11 @@ if __name__ == '__main__':
 
     # Load data
     df, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv")
+
     train_X, train_y, test_X, test_y = split_train_test(df, cancellation_labels)
 
     # Fit model over data
-    estimator = AgodaCancellationEstimator().fit(train_X, train_y)
+    estimator = AgodaCancellationEstimator().fit(train_X.to_numpy(), train_y.to_numpy())
 
     # Store model predictions over test set
-    evaluate_and_export(estimator, test_X, "id1_id2_id3.csv")
+    evaluate_and_export(estimator, test_X.to_numpy(), "try.csv")
